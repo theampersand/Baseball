0c17044b9af4b5ae806fe97199b56aef928a340e
Andrew Vincent Phillips <theampersand@gmail.com>
2013-04-21 22:41:08 -0400

    added owner module

--- owner.py
+++ owner.py
@@ -1,1 +1,17 @@
 __author__ = 'Administrator'
+
+class Owner:
+    """Represents an owner of a team in a fantasy league
+
+    attributes: id, name, team
+    """
+
+    def __init__(self, owner_id):
+        self.id = owner_id
+        self.name = None
+        self.team = None
+
+    def set_team(self, team):
+        self.team = team
+        if self not in team.owners:
+            team.add_owner(self)
